48d13cd95458882fe62b680130caaf1515912c43
Edwin Choi <edchoi@deloitte.com.au>
2020-02-03 11:22:36 +1100

    the database works and can retrieve stuff in __init__

--- Backend/core/__init__.py
+++ Backend/core/__init__.py
@@ -1,5 +1,10 @@
-#!/
+#!/usr/bin/Python3 -w
 
-import psycopg2, sqlalchemy
+from sqlalchemy import create_engine
 
+db = create_engine('postgresql+psycopg2://@localhost:5432/postgres')
 
+returns = db.connect()
+toprint = returns.execute("select * from users")
+for r in toprint:
+    print(r)
