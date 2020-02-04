de78b50afc91cacf3d3003be4a94c9e4a45f2d58
bm098182 <jomathias@deloitte.com.au>
2020-02-04 22:52:15 +1100

    lolidkm8

---
+++ react-mnssp-ui/src/components/Routes.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+// import LandingPage from "./LandingPage/LandingPage";
+import history from './history';
+
+export default class Routes extends Component {
+    render() {
+        return (
+            
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/">
+                        <SignIn />
+                    </Route>
+                    <Route path="/SignUp">
+                        <SignUp />
+                    </Route>
+                </Switch>
+            </Router>
+        )
+    }
+}
